63f3fced1b67ceb78fe6eb56d67de19389ab9e15
Aleksander Maj <aleksander.maj@gmail.com>
2019-01-01 21:01:49 +0100

    Initial Commit

---
+++ DatePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by Aleksander Maj on 01/01/2019.
+//  Copyright © 2019 DonkeyRepublic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
